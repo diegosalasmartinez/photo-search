8f0673257e513a6e8b2fd64f412bfa0fb8bd1402
Diego <diesalasmart@gmail.com>
2024-06-15 18:36:55 -0500

    feat: setup redux

---
+++ store/features/tagSlice.ts
@@ -0,0 +1,25 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+
+export interface TagSliceState {
+  query: string | null
+}
+
+const initialState: TagSliceState = {
+  query: null
+}
+
+export const tagSlice = createSlice({
+  name: "tag",
+  initialState,
+  reducers: {
+    setQuery: (state, action: PayloadAction<string>) => {
+      state.query = action.payload
+    }
+  },
+  selectors: {
+    selectQuery: (state: TagSliceState) => state.query
+  }
+})
+
+export const { setQuery } = tagSlice.actions
+export const { selectQuery } = tagSlice.selectors
---
+++ store/store.ts
@@ -0,0 +1,26 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { tagSlice } from "./features/tagSlice"
+
+// const rootReducer = combineSlices(counterSlice, quotesApiSlice)
+const rootReducer = combineSlices(tagSlice)
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware()
+    }
+  })
+}
+
+export type AppStore = ReturnType<typeof makeStore>
+export type AppDispatch = AppStore["dispatch"]
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
