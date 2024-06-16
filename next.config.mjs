fa9aef3b318195fd1a6b0d313bd37edf23ac58e8
Diego <diesalasmart@gmail.com>
2024-06-15 21:35:31 -0500

    fix: accept images from all hostnames

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**'
-    }]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
   }
 }
 
