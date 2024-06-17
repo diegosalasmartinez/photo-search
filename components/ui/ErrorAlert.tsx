4270b3b467bf91b8e0993cc01e379942fcf3fb07
Diego <diesalasmart@gmail.com>
2024-06-17 02:28:35 -0500

    style: update ui components

---
+++ components/ui/ErrorAlert.tsx
@@ -0,0 +1,15 @@
+export default function ErrorAlert({ message }: { message: string }) {
+  return (
+    <div
+      className="w-full md:w-[400px] bg-red-500 flex lg:inline-flex items-center text-indigo-100 leading-none lg:rounded-full rounded-lg px-2 py-1 mt-4"
+      role="alert"
+    >
+      <span className="flex rounded-full bg-red-800 uppercase px-2 py-1 text-xs font-bold mr-3">
+        ERROR
+      </span>
+      <span className="font-semibold mr-2 text-left flex-auto text-sm">
+        {message}
+      </span>
+    </div>
+  )
+}
