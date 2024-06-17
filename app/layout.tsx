42fece4ade68aa220784b29aecfb669de2e1c139
Diego <diesalasmart@gmail.com>
2024-06-17 13:09:58 -0500

    feat: add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import SearchTag from "@/components/SearchTag"
 import { StoreProvider } from "./store-provider"
 import "./globals.css"
+import Header from "@/components/ui/Header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <StoreProvider>
       <html lang="en">
         <body className={inter.className} suppressHydrationWarning={true}>
+          <Header />
           <main className="flex min-h-dvh flex-col p-4">
             <Suspense fallback={<></>}>
               <SearchTag />
